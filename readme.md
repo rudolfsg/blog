fb32c84427b4f3095039dfce2bb0a44de8bb52ed
rudolfsgrobins <grobinsrudolfs@gmail.com>
2023-01-04 15:44:33 +0200

    fix rss date

--- readme.md
+++ readme.md
@@ -1,14 +1,22 @@
 # Blog
 
-Simple, minimal static site generator written in Rust.
+Simple, minimal static site generator for a blog written in Rust.
 
 Features:
-* no javascript
-* code syntax highlighting with 
+* (almost) no javascript
+* code syntax highlighting with `syntect`
+* server-side rendered math equations with `katex`
+* RSS feed generation
+* Embedded jpg and png compressed with webp
+  * Optional automatic resizing by placing a `{width=50%}` tag after the embed link
+* Markdown parsing and image resizing parallelised with `rayon`
 
-## Develop
+## Usage
 
-* Don't forget to serve files from root: 
+1. Edit markdown files in `/posts`
+2. Run `cargo run --release`
+3. Check results by running a webserver.
+ Don't forget to serve files from root: 
   `cd build`
   `python3 -m http.server 1234`
   
--- src/rss.rs
+++ src/rss.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
+
 use rss::{ChannelBuilder, Item};
 use crate::post::Post;
+use chrono::{DateTime, TimeZone, NaiveDateTime, Utc, Datelike};
 
 pub fn generate_rss(posts: &Vec<Post>, blog_name: &str, blog_url: &str) -> String {
     let mut channel = ChannelBuilder::default()
@@ -10,10 +13,18 @@ pub fn generate_rss(posts: &Vec<Post>, blog_name: &str, blog_url: &str) -> Strin
 
     let mut items: Vec<Item> = Vec::new();
 
+    
+
     for post in posts {
+        // rss feeds require rfc2822 format
+        let dt = post.metadata.date; 
+        let dt = chrono::Utc.with_ymd_and_hms(dt.year(), dt.month(), dt.day(), 0, 0, 0);
+        let dt = dt.unwrap();
+
+        println!("{}", 5);
         let mut item = Item::default();
         item.set_title(post.metadata.title.clone());
-        item.set_pub_date(post.metadata.date.to_string()); 
+        item.set_pub_date(dt.to_rfc2822()); 
         item.set_link(post.metadata.url.clone());
         items.push(item); 
     }
